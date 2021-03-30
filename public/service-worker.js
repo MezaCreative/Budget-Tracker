acf4dece8e9fda79a1ff6ce698bf74d68355356f
Esteban Meza <meza.19@osu.edu>
2021-03-30 06:30:37 -0400

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/styles.css",
+    "/index.html",
+]
+
+// const CACHE_NAME = "static-cache-v2";
+// const DATA_CACHE_NAME = "data-cache-v1";
+
